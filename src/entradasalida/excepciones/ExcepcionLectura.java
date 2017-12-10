430ab1217172354a5933ad0c07df725a90c3f86c
Usuario <Usuario@DESKTOP-9SUE8IL>
2017-12-10 22:11:25 +0100

    PRACTICA 4 ENTREGA FINAL

--- src/entradasalida/excepciones/ExcepcionLectura.java
+++ src/entradasalida/excepciones/ExcepcionLectura.java
@@ -1,8 +1,29 @@
 package entradasalida.excepciones;
 
+/**
+ * The Class ExcepcionLectura.
+ * 
+ * @author Iván Mañús Murcia 48729799K
+ */
 public class ExcepcionLectura extends Exception {
+	
+	/**
+	 * Instantiates a new excepcion lectura.
+	 */
 	public ExcepcionLectura(){super();}
+	
+	/**
+	 * Instantiates a new excepcion lectura.
+	 *
+	 * @param s the s
+	 */
 	public ExcepcionLectura(String s) {super(s);}
+	
+	/**
+	 * Instantiates a new excepcion lectura.
+	 *
+	 * @param t the t
+	 */
 	public ExcepcionLectura(Throwable t) {super(t);}
 
 }
