eedcaa1ef0bf650e559b45b7a355338b7988f12d
Usuario <Usuario@DESKTOP-9SUE8IL>
2017-12-22 12:51:09 +0100

    p5

---
+++ src/entradasalida/txt/GeneradorTableroCoordenada1D.java
@@ -0,0 +1,9 @@
+package entradasalida.txt;
+
+/**
+ * The Class GeneradorTableroCoordenada1D.
+ * 
+ * @author Iván Mañús Murcia 48729799K
+ */
+public class GeneradorTableroCoordenada1D extends GeneradorFicheroPlano {
+}
---
+++ src/entradasalida/txt/GeneradorTableroCoordenada2D.java
@@ -0,0 +1,9 @@
+package entradasalida.txt;
+
+/**
+ * The Class GeneradorTableroCoordenada2D.
+ * 
+ * @author Iván Mañús Murcia 48729799K
+ */
+public class GeneradorTableroCoordenada2D extends GeneradorFicheroPlano {
+}
